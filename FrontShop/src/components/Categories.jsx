8a1dd1ee0162d612bfa680c3e6389548a7fe57b5
Thibaud Mathis <thibaud.mathis01@gmail.com>
2021-09-19 12:36:50 +0200

    add categories

--- FrontShop/src/components/Categories.jsx
+++ FrontShop/src/components/Categories.jsx
@@ -3,14 +3,20 @@ import styled from 'styled-components'
 import CategoryItem from './CategoryItem'
 import { categories } from '../data'
 
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+`
+
 const Categories = () => {
     return (
         <Container>
             {
-                categories.map((item) => {
+                categories.map((item) => (
                     <CategoryItem item={item}/>
-                })
+                ))
             }
+
         </Container>
     )
 }
--- FrontShop/src/components/CategoryItem.jsx
+++ FrontShop/src/components/CategoryItem.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const Info = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    /* background: orange; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+const Button = styled.button`
+    border: none;
+    padding: 10px;
+    background: white;
+    color: gray;
+    cursor: pointer;
+    font-weight: 600;
+`
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`
+
 
 const CategoryItem = ({ item }) => {
     return (
         <Container>
-            <h1>{item.title}</h1>
+            <Image src={item.img}/>
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
         </Container>
     )
 }
