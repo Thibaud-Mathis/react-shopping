7b3dce947eaf75075b76d06bc415d36f6f4a87a5
Thibaud Mathis <thibaud.mathis01@gmail.com>
2021-09-19 18:24:37 +0200

    add footer

---
+++ FrontShop/src/components/Footer.jsx
@@ -0,0 +1,108 @@
+import React from 'react'
+import styled from "styled-components"
+import { Facebook, Twitter, Instagram, Room, Phone, MailOutline } from "@material-ui/icons"
+
+const Container = styled.div`
+    display: flex;
+    /* background: #e6e0e0; */
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+
+const Right = styled.div`
+    flex: 1;
+`
+const Logo = styled.h1`
+    font-weight: 800;
+`
+const Description = styled.p`
+    margin: 20px 0;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    background: #${props => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: white;
+    margin-right: 20px;
+`
+
+const ContactItem = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    width: 100%;
+`
+const Payement = styled.img``
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>LOCOS</Logo>
+                <Description>Lorem ipsum dolor sit amet consectetur adipisicing elit. Que explicabo ad facere cumque at ratione libero! Laboriosam, atque est.</Description>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter/>
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Usefull links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>bla</ListItem>
+                    <ListItem>bla</ListItem>
+                    <ListItem>bla</ListItem>
+                    <ListItem>bla</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><Room style={{marginRight: "10px"}}/>62, Rue du bouchaunoix</ContactItem>
+                <ContactItem><Phone style={{marginRight: "10px"}}/>+04 01 03 45 12</ContactItem>
+                <ContactItem><MailOutline style={{marginRight: "10px"}}/>contact@company.com</ContactItem>
+                <Payement src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer;
---
+++ FrontShop/src/components/Newsletter.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Send } from '@material-ui/icons'
+
+
+const Container = styled.div`
+    height: 60vh;
+    background: #fcf5f5;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+const Title = styled.h1`
+    font-size: 70px;
+`
+const Description = styled.div`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+`
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+`
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+`
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background: teal;
+    color: white;
+`
+
+const Newsletter = () => {
+    return (
+        <Container>
+            <Title>Newsletter</Title>
+            <Description>Hear from us and get our best discounts before everyone else !</Description>
+            <InputContainer>
+                <Input placeholder="Email" />
+                <Button>
+                    <Send/>
+                </Button>
+            </InputContainer>
+        </Container>
+    )
+}
+
+export default Newsletter
